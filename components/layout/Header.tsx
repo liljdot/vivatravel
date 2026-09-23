422365347d522a498a88e800ddc3b93cdda2714b
liljdot <jehuoki@gmail.com>
2026-09-23 13:21:12 +0100

    feat: implement responsive Header with Drawer and Accordion components; add Drawer functionality for mobile navigation

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,19 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/assets/images/viva-logo.png"
 import { whatsappLink } from "@/app/data";
 import { BsTelephone } from "react-icons/bs";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, Button, Drawer, DrawerContent, DrawerTrigger } from "../ui";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { FiMenu } from "react-icons/fi";
+import { IoClose } from "react-icons/io5";
 
 const Header: React.FC = () => {
+    const [sandwichOpen, setSandwichOpen] = useState(false)
+    const pathname = usePathname()
 
     return (
         <>
@@ -15,13 +24,14 @@ const Header: React.FC = () => {
                         <Image
                             alt="mdl logo"
                             src={logo}
-                            className="w-15 md:w-50 h-15 md:h-24"
+                            className="w-19 md:w-50 h-12 md:h-24"
                         />
                     </Link>
                 </div>
 
                 {/* navbar center */}
-                <div className="navbar-center hidden lg:flex">
+                {/* hidden on mobile, show on desktop */}
+                <div className="navbar-center hidden md:flex">
                     <ul className="menu menu-horizontal gap-14 text-base px-1">
                         <li><Link href="/" className="hover:text-primary hover:font-medium">Home</Link></li>
                         <li>
@@ -40,10 +50,51 @@ const Header: React.FC = () => {
                 </div>
                 {/* navbar end */}
                 <div className="navbar-end">
-                    <Link href={whatsappLink} className="btn rounded-full bg-primary font-normal px-5 py-4 transition-all duration-300 ease-in-out hover:scale-105" target="_blank" rel="noopener noreferrer">
+                    {/* hidden on mobile, show on desktop */}
+                    <Link href={whatsappLink} className="hidden md:flex btn rounded-full bg-primary font-normal px-5 py-4 transition-all duration-300 ease-in-out hover:scale-105" target="_blank" rel="noopener noreferrer">
                         Contact Us
                         <BsTelephone className="ml-2 size-4.5 text-secondary" />
                     </Link>
+
+                    {/* show on mobile, hidden on desktop */}
+                    <div className="flex md:hidden">
+                        <Drawer key={pathname} swipeDirection="up" onOpenChange={setSandwichOpen}>
+                            <DrawerTrigger
+                                render={<Button className={"text-primary"} variant="ghost" />}
+                            >
+                                <FiMenu className={`absolute size-6 text-primary ${sandwichOpen ? 'opacity-0 rotate-180' : 'opacity-100'} transition-all ease-in-out duration-700`} />
+                                <IoClose className={`absolute size-6 text-primary ${sandwichOpen ? 'opacity-100' : 'opacity-0 -rotate-180'} transition-all ease-in-out duration-500`} />
+                            </DrawerTrigger>
+                            <DrawerContent className={`pt-14`}>
+                                <div className="flex flex-col py-12 bg-neutral text-base px-4.5 rounded-3xl">
+                                    <ul className="flex flex-col text-neutral-content space-y-11">
+                                        <li><Link href={"/"}>Home</Link></li>
+                                        <li>
+                                            <Accordion>
+                                                <AccordionItem value="products" className="gap-0">
+                                                    <AccordionTrigger className="decoration-0 p-0">Services</AccordionTrigger>
+
+                                                    <AccordionContent className="px-5 pt-11">
+                                                        <ul className="flex flex-col space-y-11">
+                                                            <li><a className="decoration-0" >Travel & Tourism</a></li>
+                                                            <li><a className="decoration-0">Business Growth</a></li>
+                                                            <li><a className="decoration-0">Overseas Opportunities</a></li>
+                                                            <li><a className="decoration-0">International Healthcare</a></li>
+                                                        </ul>
+                                                    </AccordionContent>
+                                                </AccordionItem>
+                                            </Accordion>
+                                        </li>
+                                        <li><Link href={"/about"}>About</Link></li>
+                                        <Link href={whatsappLink} className="btn rounded-full bg-primary font-normal px-5 py-4 transition-all duration-300 ease-in-out hover:scale-105" target="_blank" rel="noopener noreferrer">
+                                            Contact Us
+                                            <BsTelephone className="ml-2 size-4.5 text-secondary" />
+                                        </Link>
+                                    </ul>
+                                </div>
+                            </DrawerContent>
+                        </Drawer>
+                    </div>
                 </div>
             </div>
         </>
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,2 +1,4 @@
 export * from "./button"
 export * from "./tabs"
+export * from "./drawer"
+export * from "./accordion"
